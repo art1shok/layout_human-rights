a80489f0527966684a6811fd7f09bde855dda3cd
art1shok <kvitkaart554@gmail.com>
2019-10-21 11:59:43 +0300

    deploy static content

---
+++ report/html_report/config.js
@@ -0,0 +1,256 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20191021-115743\\Entire_document.png",
+        "selector": "document",
+        "fileName": "Entire_document.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 102
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\H1_heading.png",
+        "test": "..\\bitmaps_test\\20191021-115743\\H1_heading.png",
+        "selector": "h1",
+        "fileName": "H1_heading.png",
+        "label": "H1 heading",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\H2_heading.png",
+        "test": "..\\bitmaps_test\\20191021-115743\\H2_heading.png",
+        "selector": "h2:first-of-type",
+        "fileName": "H2_heading.png",
+        "label": "H2 heading",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Paragraph.png",
+        "test": "..\\bitmaps_test\\20191021-115743\\Paragraph.png",
+        "selector": "p:first-of-type",
+        "fileName": "Paragraph.png",
+        "label": "Paragraph",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ordered_list.png",
+        "test": "..\\bitmaps_test\\20191021-115743\\Ordered_list.png",
+        "selector": "ol:first-of-type",
+        "fileName": "Ordered_list.png",
+        "label": "Ordered list",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -15
+          },
+          "misMatchPercentage": "22.01",
+          "analysisTime": 50
+        },
+        "diffImage": "..\\bitmaps_test\\20191021-115743\\failed_diff_Ordered_list.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ordered_list.png",
+        "test": "..\\bitmaps_test\\20191021-115743\\Ordered_list.png",
+        "selector": "ol:first-of-type.__n1",
+        "fileName": "Ordered_list.png",
+        "label": "Ordered list",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -15
+          },
+          "misMatchPercentage": "22.01",
+          "analysisTime": 65
+        },
+        "diffImage": "..\\bitmaps_test\\20191021-115743\\failed_diff_Ordered_list.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ordered_list.png",
+        "test": "..\\bitmaps_test\\20191021-115743\\Ordered_list.png",
+        "selector": "ol:first-of-type.__n2",
+        "fileName": "Ordered_list.png",
+        "label": "Ordered list",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -15
+          },
+          "misMatchPercentage": "22.01",
+          "analysisTime": 60
+        },
+        "diffImage": "..\\bitmaps_test\\20191021-115743\\failed_diff_Ordered_list.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ordered_list.png",
+        "test": "..\\bitmaps_test\\20191021-115743\\Ordered_list.png",
+        "selector": "ol:first-of-type.__n3",
+        "fileName": "Ordered_list.png",
+        "label": "Ordered list",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -15
+          },
+          "misMatchPercentage": "22.01",
+          "analysisTime": 44
+        },
+        "diffImage": "..\\bitmaps_test\\20191021-115743\\failed_diff_Ordered_list.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Ordered_list.png",
+        "test": "..\\bitmaps_test\\20191021-115743\\Ordered_list.png",
+        "selector": "ol:first-of-type.__n4",
+        "fileName": "Ordered_list.png",
+        "label": "Ordered list",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -15
+          },
+          "misMatchPercentage": "22.01",
+          "analysisTime": 65
+        },
+        "diffImage": "..\\bitmaps_test\\20191021-115743\\failed_diff_Ordered_list.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Articles_17_to_30_link.png",
+        "test": "..\\bitmaps_test\\20191021-115743\\Articles_17_to_30_link.png",
+        "selector": "a:first-of-type",
+        "fileName": "Articles_17_to_30_link.png",
+        "label": "Articles 17 to 30 link",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/human-rights/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
